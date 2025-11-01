3aebd960e0e38261c41cf7fbf27d34587292935a
AragornOfKebroyd <86985645+AragornOfKebroyd@users.noreply.github.com>
2025-11-01 16:19:10 +0000

    serve image files

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect
 import requests
 import os
+import json
+import chess
 # from ../scripts/generate_board import generate_board_image
 
 app = Flask(__name__)
@@ -36,6 +38,37 @@ def move():
     # generate_board_image(fen=state["board"], output_path="../board_image.png")
     
     return redirect(redirect_url)
+
+@app.route("/click")
+def click():
+    square = request.args.get("sq")
+    game = request.args.get("game")
+
+    # read the board state and return
+
+pieceEnum = {chess.BISHOP: 'b', chess.KING: 'k', chess.KNIGHT: 'n', chess.PAWN: 'p', chess.QUEEN: 'q', chess.ROOK: 'r'}
+colourEnum = {chess.BLACK: 'd', chess.WHITE: 'l'}
+
+@app.route("/display")
+def display():
+    # return an image grid square
+    square = request.args.get("sq")
+    
+    game = request.args.get("game")
+
+    with open('state.json', 'r') as f:
+        state = json.load(f)
+    
+    board = chess.Board(state["board"])
+
+    chess_square = chess.parse_square(square)
+    piece = board.piece_at(chess_square)
+    board_colour = board.color_at(chess_square)
+
+    imagename = pieceEnum[piece.piece_type] + colourEnum[piece.color] + colourEnum[board_colour]
+
+    return app.send_static_file(f'./images/{imagename}.png')    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
